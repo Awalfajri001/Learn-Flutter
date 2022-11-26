d6391c4fbf0dc3f1fa6c11392716b277d32fffa2
Awalfajri001 <116197604+Awalfajri001@users.noreply.github.com>
2022-11-26 20:25:20 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_awal/home_page.dart';
+import 'package:flutter_awal/profile_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const rootPage(),
+    );
+  }
+}
+
+// ignore: camel_case_types
+class rootPage extends StatefulWidget {
+  const rootPage({super.key});
+
+  @override
+  State<rootPage> createState() => rootPageState();
+}
+
+class rootPageState extends State<rootPage> {
+  int currentPage = 0;
+  List<Widget> pages = const [HomePage(), ProfilePage()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter'),
+      ),
+      body: pages[currentPage],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('floating Action Button');
+        },
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profile')
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
+      ),
+    );
+  }
+}
